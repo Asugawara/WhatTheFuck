d1c60184af806e2f4dd398632cf22fb38563a012
Asugawara <asgasw@gmail.com>
2025-01-27 20:27:12 +0900

    refactor: websearch.py

--- src/wtf/pipelines/components/websearch.py
+++ src/wtf/pipelines/components/websearch.py
@@ -9,33 +9,53 @@ from requests_html_playwright import HTMLSession
 class WebSearch:
     GOOGLE_SEARCH_BASEURL = "https://www.google.com/search"
 
-    def __init__(self, available_netlocs: tuple[str, ...] | None = None, max_urls: int = 5) -> None:
-        self._available_netlocs = available_netlocs
+    def __init__(
+        self, allowed_domains: tuple[str, ...] | None = None, top_k: int = 10, user_agent: str = "", max_urls: int = 5
+    ) -> None:
+        self._allowed_domains = allowed_domains
+        self._top_k = top_k
         self._max_urls = max_urls
+        self._user_agent = user_agent
 
-    @component.output_types(urls=list[str])  # type: ignore
+    @staticmethod
+    def _is_allowed_url(url: str, allowed_domains: tuple[str, ...] | None) -> bool:
+        if allowed_domains is None:
+            logger.debug("No allowed domains")
+            return True
+
+        url_components = urlparse(url)
+        if (
+            url_components.netloc
+            and allowed_domains
+            and any(url_components.netloc in domain for domain in allowed_domains)
+        ):
+            return True
+        logger.debug("Skip the url with netloc: %s", url_components.netloc)
+        return False
+
+    @component.output_types(links=list[str])  # type: ignore
     def run(self, query: str) -> dict[str, list[str]]:
         logger.debug("query: %s", query)
         with HTMLSession() as session:
-            response = session.get(self.GOOGLE_SEARCH_BASEURL, params={"q": query})
+            response = session.get(
+                self.GOOGLE_SEARCH_BASEURL,
+                params={"q": query},
+                headers={"User-Agent": str(self._user_agent), "Referer": "https://www.google.com"},
+            )
             response.html.render()
 
-        urls = []
+        links = []
         counter = 0
         for url in response.html.absolute_links:
             logger.debug("url: %s", url)
             if url.startswith(self.GOOGLE_SEARCH_BASEURL):
                 logger.debug("Skip the search result page")
                 continue
-            url_components = urlparse(url)
-            if not url_components.netloc:
+            if not self._is_allowed_url(url, self._allowed_domains):
                 continue
-            if self._available_netlocs and url_components.netloc not in self._available_netlocs:
-                logger.debug("Skip the url with netloc: %s", url_components.netloc)
-                continue
-            urls.append(url)
+            links.append(url)
             counter += 1
             if counter >= self._max_urls:
                 break
 
-        return {"urls": urls}
+        return {"links": links[: self._top_k]}
