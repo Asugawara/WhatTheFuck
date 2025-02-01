e3dadbf449911fea8221c5dae01b1d31f356bbd0
Asugawara <asgasw@gmail.com>
2025-02-01 15:06:33 +0900

    bug: add zsh support

--- src/wtf/shells/__init__.py
+++ src/wtf/shells/__init__.py
@@ -3,6 +3,7 @@ import os
 from wtf.shells.base import ShellBase
 from wtf.shells.bash import BashShell
 from wtf.shells.fish import FishShell
+from wtf.shells.zsh import ZshShell
 
 
 def factroy_shell() -> ShellBase:
@@ -11,4 +12,6 @@ def factroy_shell() -> ShellBase:
         return BashShell()
     if shell_path.endswith("fish"):
         return FishShell()
+    if shell_path.endswith("zsh"):
+        return ZshShell()
     raise NotImplementedError("Only `bash` or `fish` shell are supported")
